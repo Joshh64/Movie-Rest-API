d80e9165036b598b45de4fff057953657b9c50de
Joshh64 <joshandrewfrancis@gmail.com>
2023-06-06 15:54:06 +0100

    added fix for CORS Policy error

--- src/server.js
+++ src/server.js
@@ -6,6 +6,14 @@ const port = process.env.PORT || 5001
 const userRouter = require("./users/routes")
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}); // removes the CORS policy error
+
 app.use(userRouter)
 
 app.get("/health", (req, res) => {
