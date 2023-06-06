e3fb5fbef269b206537cb0b5c145cb4565d43af3
Joshh64 <joshandrewfrancis@gmail.com>
2023-06-06 16:08:52 +0100

    fixed cors policy error

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 require("dotenv").config()
+const cors = require("cors")
 const app = express()
 
 const port = process.env.PORT || 5001
@@ -7,12 +8,7 @@ const userRouter = require("./users/routes")
 
 app.use(express.json())
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-}); // removes the CORS policy error
+app.use(cors()) // enable requests from any origin (fixes CORS policy error)
 
 app.use(userRouter)
 
